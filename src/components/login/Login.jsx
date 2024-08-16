1c4ef1afac68f8f81639ab225935d2c8de096ea6
utsavkalathiya1602 <157691067+utsavkalathiya1602@users.noreply.github.com>
2024-08-16 15:58:41 +0530

    Update Login.jsx

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -36,14 +36,14 @@ function Login({ setIsLoggedIn }) {
       return;
     }
 
-    try {
-      const response = await fetch('http://localhost/project/login.php', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password }),
-      });
+    // try {
+    //   const response = await fetch('http://localhost/project/login.php', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({ username, password }),
+    //   });
 
       const data = await response.json();
 
